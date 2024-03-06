bb7128a725e116172ffca8cf041df8ccc55195d5
Shreyash Shastre <158807601+ShreyashShastre@users.noreply.github.com>
2024-03-06 21:39:17 +0530

    Update app.py

--- app.py
+++ app.py
@@ -2,7 +2,6 @@ import os
 import pickle
 import streamlit as st
 from streamlit_option_menu import option_menu
-from win10toast import ToastNotifier
 
 # Set page configuration
 st.set_page_config(page_title="Health Assistant",
